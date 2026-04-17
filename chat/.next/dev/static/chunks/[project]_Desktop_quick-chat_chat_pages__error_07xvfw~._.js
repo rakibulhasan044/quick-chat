a1b1f701ccd88a623afd093713aa1e66ed5bcf34
rakibulhasan044 <rhrakib044@gmail.com>
2026-04-17 16:45:56 +0600

    google login

---
+++ chat/.next/dev/static/chunks/[project]_Desktop_quick-chat_chat_pages__error_07xvfw~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0p~c_next_dist_compiled_061djl6._.js","static/chunks/0p~c_next_dist_shared_lib_0qpmsra._.js","static/chunks/0p~c_next_dist_client_11hd4n3._.js","static/chunks/0p~c_next_dist_0_5amue._.js","static/chunks/0kn__next_dist_pages__error_113i0uz.js","static/chunks/[next]_entry_page-loader_ts_0_t0n48._.js","static/chunks/0p~c_react-dom_0_.1o2z._.js","static/chunks/0p~c_11vvo8d._.js","static/chunks/[root-of-the-server]__0~vea70._.js"],
+    source: "entry"
+});
---
+++ chat/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/0p~c_next_dist_compiled_061djl6._.js",
+  "static/chunks/0p~c_next_dist_shared_lib_0qpmsra._.js",
+  "static/chunks/0p~c_next_dist_client_11hd4n3._.js",
+  "static/chunks/0p~c_next_dist_0_5amue._.js",
+  "static/chunks/0kn__next_dist_pages__error_113i0uz.js",
+  "static/chunks/[next]_entry_page-loader_ts_0_t0n48._.js",
+  "static/chunks/0p~c_react-dom_0_.1o2z._.js",
+  "static/chunks/0p~c_11vvo8d._.js",
+  "static/chunks/[root-of-the-server]__0~vea70._.js",
+  "static/chunks/[project]_Desktop_quick-chat_chat_pages__error_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_Desktop_quick-chat_chat_pages__error_06si0f3._.js"
+])
