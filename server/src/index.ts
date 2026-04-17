c51f42b01a0dcf035fbf79567a30f46b1e3a3ea7
rakibulhasan044 <rhrakib044@gmail.com>
2026-04-17 15:09:08 +0600

    home page

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import type { Application, Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
+const app: Application = express();
+const PORT = process.env.PORT || 7000;
+import Routes from "./routes/index.js";
+
+// * Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("It's working 🙌");
+});
+
+app.use("/api", Routes)
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
